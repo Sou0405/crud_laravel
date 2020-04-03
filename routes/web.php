f7431487b219ec0e6038a6f23caa8be2a0fa2429
jon <jon@localhost.localdomain>
2020-04-03 10:25:25 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('todo')->group(function(){
+	//index
+	Route::get('/', 'ToDoController@index')->name('todo.index');
+
+	//show
+	Route::get('{id}', 'TodoController@show')
+		->where('id', '[0-9]+')
+		->name('todo.show');
+
+	//create
+	Route::get('/create', 'ToDoController@create')
+		->name('todo.create');
+	Route::post('create', 'ToDoController@store')
+		->name('todo.store');
+
+	//edit 
+	Route::get('/{id}/edit', 'ToDoController@edit')
+		->where('id', '[0-9]+')
+		->name('todo.edit');
+	Route::post('/{id}/update', 'ToDoController@update')
+		->where('id', '[0-9]+')
+		->name('todo.update');
+
+
+
+
+
+})
